69b569011bef2d97d422b13184cb0402d19f594f
Serdar Can <45050982+srdrcn@users.noreply.github.com>
2021-01-23 13:58:55 +0300

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import boto3
+s3_client = boto3.client("s3")
+dynamodb = boto3.resource('dynamodb')
+table =dynamodb.Table('employees')
+
+
+def lambda_handler(event, context):
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    s3_file_name = event['Records'][0]['s3']['object']['key']
+    resp = s3_client.get_object(Bucket=bucket_name,Key=s3_file_name)
+    data = resp['Body'].read().decode("utf-8",errors="replace")
+    employees = data.split("\n")
+    for emp in employees:
+        print(emp)
+        emp_data = emp.split(",")
+        # Add it to dynomodb
+        try:
+              table.put_item(
+            Item = { 
+                
+                "id": emp_data[0],
+                "name": emp_data[1],
+                "location": emp_data[2]
+                 }
+             
+            )
+        except Exception as e:
+          print("End of file")
+     
